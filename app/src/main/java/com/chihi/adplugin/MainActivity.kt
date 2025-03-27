6e5cb8a43264b5e4627e5c368c31eef0f8f11c80
CoderZeng <chienzy@yeah.net>
2025-03-27 16:20:49 +0800

    添加测试代码

--- app/src/main/java/com/chihi/adplugin/MainActivity.kt
+++ app/src/main/java/com/chihi/adplugin/MainActivity.kt
@@ -10,7 +10,28 @@ class MainActivity : Activity() {
         setContentView(R.layout.activity_main)
         PluginManager.init(this.applicationContext)
         Handler().postDelayed({
-            AdPlugin.initialize()
+            //AdPlugin.initialize()
+            println("开始反射")
+            callAdPluginInitialize()
         },3000)
+    }
+
+    private fun callAdPluginInitialize() {
+        try {
+            // 1. 获取 ClassLoader（替换为您的实际 ClassLoader）
+            // 方式1：使用当前类加载器（适用于主工程中的类）
+            //val clazz = Class.forName("com.chihi.adplugin.AdPlugin")
+            val clazz = Class.forName("com.android.fungo.adplugin.AdPlugin")
+
+            // 情况2：调用实例方法
+            val instance = clazz.getDeclaredConstructor().newInstance()
+            val instanceMethod = clazz.getMethod("initialize")
+            instanceMethod.invoke(instance)
+            println("类加载成功: ${clazz.simpleName}")
+            println("初始化完成")
+
+        } catch (e: Exception) {
+            println("异常了: ${e.message}")
+        }
     }
 }
